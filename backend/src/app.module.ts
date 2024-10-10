a27fbbb7a6cd2b737db34af3e15c1a807f3b1369
sratslla <shubhgaur.cse24@jecrc.ac.in>
2024-10-11 05:21:15 +0530

    trial4

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { AdminModule } from './admin/admin.module';
-import { MongooseModule, MongooseModuleFactoryOptions } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AdminService } from './admin/admin.service';
 import { TelegramBotService } from './telegram-bot';
 import { config } from 'dotenv';
@@ -17,10 +17,11 @@ config();
     DatabaseModule,
     AdminModule,
     MongooseModule.forRootAsync({
-      useFactory: () =>
-        ({
-          uri: 'mongodb+srv://allstars:GsaUjSNg5AmivqEe@cluster0.hjmgq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-        }) as MongooseModuleFactoryOptions,
+      useFactory: () => ({
+        uri: 'mongodb+srv://allstars:GsaUjSNg5AmivqEe@cluster0.hjmgq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
     }),
   ],
   controllers: [AppController],
